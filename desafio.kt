baf6c1e23bc8329264c8c52d0547da30f96d70e2
ALLYSSON CIDADE <120651526+AllyssonCidade@users.noreply.github.com>
2025-11-11 15:36:50 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,50 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val nome: String)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formacao(
+    val nome: String,
+    var conteudos: List<ConteudoEducacional>,
+    val nivel: Nivel
+) {
+    private val _inscritos = mutableListOf<Usuario>()
+    val inscritos: List<Usuario> = _inscritos
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuarios: Usuario) {
+        usuarios.forEach {
+            if (it !in _inscritos) {
+                _inscritos += it
+                println("${it.nome} matriculado em $nome!")
+            } else {
+                println("${it.nome} já está matriculado.")
+            }
+        }
     }
+
+    fun totalHoras() = conteudos.sumOf { it.duracao }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val kubernets = ConteudoEducacional("Kubernetes", 40)
+    val aws = ConteudoEducacional("AWS", 45)
+    val docker = ConteudoEducacional("Docker", 32)
+
+    val formacaoDevOps = Formacao(
+        nome = "DevOps",
+        conteudos = listOf(kubernets, aws, docker),
+        nivel = Nivel.INTERMEDIARIO
+    )
+
+    val user1 = Usuario("Allysson")
+    formacaoDevOps.matricular(user1)
+
+    val user2 = Usuario("Leila")
+    formacaoDevOps.matricular(user1, user2)
+
+    println("\n=== Inscritos na formação ${formacaoDevOps.nome} ===")
+    formacaoDevOps.inscritos.forEach { println("- ${it.nome}") }
+
+    println("\nDuração total: ${formacaoDevOps.totalHoras()} minutos")
 }
